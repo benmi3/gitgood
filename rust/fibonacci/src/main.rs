f091c289665d224a8a5909aeabd663021b1532a1
Benjamin Jørgensen <me@benmi.me>
2023-11-01 22:24:56 +0900

    trying to think out my own fibancci function

--- rust/fibonacci/src/main.rs
+++ rust/fibonacci/src/main.rs
@@ -1,13 +1,32 @@
+use std::process::exit;
+
 fn main() {
     println!("Hello, world!");
-    let mut int_array = vec![0];
+    let mut int_array = vec![0, 1, 1];
 
     do_fibonacci(&mut int_array)
 }
 
-fn do_fibonacci(int_array: &mut Vec<i8>) {
-    if int_array.len() > 1 {
-        int_array.push(0)
+fn do_fibonacci(int_array: &mut Vec<i64>) {
+    if int_array.len() <= 1 {
+        println!("There needs to be 2 or more items in this list");
+        exit(0)
     };
-    println!("this is the new array :{:?}", int_array)
+    int_array.sort();
+    println!("this is the new array :{:?}", &int_array);
+
+    let mut counter = 0;
+
+    loop {
+        counter += 1;
+        let x = int_array[counter];
+        let y = int_array[1 + counter];
+        let new_num = x + y;
+        println!("new number is :{:?}", &new_num);
+        int_array.push(new_num);
+
+        if counter == 10 {
+            break;
+        }
+    }
 }
