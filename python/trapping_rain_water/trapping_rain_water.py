b13d7d87c86bb645320fb57545ee6ae152c0b74b
Benjamin Jørgensen <me@benmi.me>
2023-11-13 14:57:09 +0900

    made my own trapping_rain_water solution in python

--- python/trapping_rain_water/trapping_rain_water.py
+++ python/trapping_rain_water/trapping_rain_water.py
@@ -1,39 +1,21 @@
-
 class Solution:
     def trap(self, height: list[int]) -> int:
         water_level = 0
-        print(f"{height}")
         check_ = self.thinking_box(height)
-        print(f"{check_}")
-        lets_see = 0
-        lets_see_2 = 0
+        amount_of_water = 0
         for index, item in enumerate(check_):
-            print(f"{item}")
             if True in item:
-                for index_, item_ in enumerate(item):
-                    if index_ > 0:
-                        if not item_:
-                            if item[index_ - 1]:
-                                if index_ < len(item):
-                                    lets_see = lets_see + 1
-                # -------------------------------------
-                    if not item_:
-                        if not index_ == 0:
-                            print("yay")
-                            for x in range(len(item)):
-                                if item[index_ - x]:
-                                    lets_see_2 = lets_see_2 + 1
-                                    break
-
-        print(f"lets_see_2: {lets_see_2}")
-        print(f"lets_see: {lets_see}")
-        return water_level
+                amount_of_water = (
+                    amount_of_water
+                    + self.thinking_outside(item)
+                )
+        return amount_of_water
 
     def thinking_box(self, h_: list[int]) -> list[bool]:
         list_clone = h_
         list_looper = False
         list_of_bools = []
-        while list_looper == False:
+        while not list_looper:
             internal_list = []
             for index_, item in enumerate(list_clone):
                 if item == 0:
@@ -44,14 +26,28 @@ class Solution:
             list_of_bools.append(internal_list)
             if True not in internal_list:
                 list_looper = True
-                # break
-        return list_of_bools # reversed(list_of_bools)
+        return list_of_bools
+
+    def thinking_outside(self, h_: list[bool]) -> int:
+        wall_finder = []
+        between_walls = []
+        for index, item in enumerate(h_):
+            if item:
+                wall_finder.append(index)
+        wall_finder.sort()
+        for index, item in enumerate(h_):
+            if not item:
+                if wall_finder[0] < index:
+                    if index < wall_finder[-1]:
+                        if index not in between_walls:
+                            between_walls.append(index)
+
+        return len(between_walls)
 
 
 if __name__ == "__main__":
-    # height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-    height = [4, 2, 0, 3, 2, 5]
+    height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]  # this should return 6
+    height = [4, 2, 0, 3, 2, 5]                    # this should return 9
     s = Solution()
     water_level = s.trap(height)
-    print("----------Finished measuring--------------")
-    print(water_level)
+    print(f"---Finished measuring---\nWater Level = {water_level}")
