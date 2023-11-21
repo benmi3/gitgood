cac12a8bd4051dcbbf8e50f5094e47be56ef27f4
Benjamin Jørgensen <me@benmi.me>
2023-11-21 13:02:50 +0900

    added hi to java

--- java/simple_web_server/src/main/java/com/example/test/TestApplicationController.java
+++ java/simple_web_server/src/main/java/com/example/test/TestApplicationController.java
@@ -1,12 +1,18 @@
 package com.example.test;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestApplicationController {
 
     @RequestMapping("/")  // Set current method as root
-    public String test() {
-        return "Hello World";
+    public String response_root() {
+        return "Hello World \nThis is the root page";
+    }
+
+    @RequestMapping("/hi")  // Set current method as root
+    public String hello(@RequestParam(value = "name", defaultValue = "User") String name) {
+      return String.format("Hi %s! \nThis is the hi page", name);
     }
 }
