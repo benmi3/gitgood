dfc153dd82147f44309410447b8dc8d096518fdb
Benjamin Jørgensen <me@benmi.me>
2024-01-23 10:29:05 +0900

    created container with most water function, but the one I created, with loop within a loop was slow, so I also used ai to create a new more optimized version aswell. bot are in the same file.

--- c++/snippets/container_with_most_water.cpp
+++ c++/snippets/container_with_most_water.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -54,7 +55,7 @@ public:
     return max;
   }
 
-  int maxArea1(vector<int> &height) {
+  int maxArea2(vector<int> &height) {
     int max = 0;
     int num1 = 0;
     int num2 = 0;
@@ -101,13 +102,90 @@ public:
       return check_area(height[0], num3, num4, 0);
     return max;
   }
+
+  int maxArea3(vector<int> &height) {
+    // This works, but is slow
+    /*
+    int max = 0;
+    int tmp = 0;
+    int dis = 0;
+    for (int x = 0; x < height.size() - 1; ++x) {
+      // Outer
+      for (int y = x + 1; y < height.size(); ++y) {
+        // Inner
+        dis = y - x;
+        if (height[x] > height[y]) {
+          tmp = height[y] * dis;
+        } else {
+          tmp = height[x] * dis;
+        }
+        if (tmp > max) {
+          max = tmp;
+        }
+      }
+    }
+    */
+    int num_big = 0;
+    int num_big1 = 0;
+    int num_big2 = 0;
+    int num_ind = 0;
+    int num_ind1 = 0;
+    int num_ind2 = 0;
+    for (int x = 0; x < height.size() - 1; ++x) {
+      if (height[x] > num_big1) {
+        num_big2 = num_big1;
+        num_ind2 = num_ind1;
+        num_big1 = height[x];
+        num_ind1 = x;
+      } else if (height[x] > num_big2) {
+        num_big2 = height[x];
+        num_ind2 = x;
+      }
+    }
+    if (num_big1 > num_big2) {
+      num_big = num_big2;
+    } else {
+      num_big = num_big1;
+    }
+
+    if (num_ind1 > num_ind2) {
+      num_ind = num_ind1 - num_ind2;
+    } else {
+      num_ind = num_ind2 - num_ind1;
+    }
+
+    return num_big * num_ind;
+    // return max;
+  }
+
+  int maxArea1(vector<int> &height) {
+    // gpt optimized
+    int maxArea = 0;
+    int left = 0;
+    int right = height.size() - 1;
+
+    while (left < right) {
+      int h = min(height[left], height[right]);
+      int w = right - left;
+      int area = h * w;
+      maxArea = max(maxArea, area);
+
+      if (height[left] < height[right]) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+
+    return maxArea;
+  }
 };
 
 int main(int argc, char *argv[]) {
   Solution tester;
   vector<int> val1 = {1, 2, 4, 3};
   vector<int> val2 = {1, 2, 1};
-  vector<int> val3 = {1,8,100,2,100,4,8,3,7};
+  vector<int> val3 = {1, 8, 100, 2, 100, 4, 8, 3, 7};
   vector<int> val4 = {1, 8, 6, 2, 5, 4, 8, 25, 7};
   vector<int> val5 = {2, 3, 4, 5, 18, 17, 6};
   vector<int> val6 = {8, 20, 1, 2, 3, 4, 5, 6};
@@ -123,5 +201,14 @@ int main(int argc, char *argv[]) {
   cout << "A4: " << a4 << endl;
   cout << "A5: " << a5 << endl;
   cout << "A6: " << a6 << endl;
+  /*
+  A1: 4
+  A2: 2
+  A3: 200
+  A4: 49
+  A5: 17
+  A6: 42
+    */
+
   return 0;
 }
