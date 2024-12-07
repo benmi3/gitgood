4f9097795c189287edabfa0cc82c87687b6dc70c
Benjamin Jørgensen <me@benmi.me>
2024-12-07 20:40:32 +0900

    djangotutorial before detail cleanup

--- python/lets_remember_django/djangotutorial/polls/views.py
+++ python/lets_remember_django/djangotutorial/polls/views.py
@@ -3,21 +3,22 @@ from django.shortcuts import render
 from .models import Question
 
 # Create your views here.
-from django.http import HttpResponse, HttpRequest
-from django.template import loader
+from django.http import HttpResponse, HttpRequest, Http404
 
 
 def index(request: HttpRequest):
     print(request.method)
     latest_question_list = Question.objects.order_by("-pub_date")
-    template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 
 def detail(request: HttpRequest, question_id: int) -> HttpResponse:
-    print(request)
-    return HttpResponse(b"You're looking at the question." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request: HttpRequest, question_id: int) -> HttpResponse:
