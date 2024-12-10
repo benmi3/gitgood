88b23b574cdc7357ee2fa18874b6b6ff7730fdcd
Benjamin Jørgensen <me@benmi.me>
2024-12-10 21:11:07 +0900

    djangotutorial simple tests example

--- python/lets_remember_django/polls/tests.py
+++ python/lets_remember_django/polls/tests.py
@@ -1,3 +1,32 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
 
 # Create your tests here.
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        futute_question = Question(pub_date=time)
+        self.assertIs(futute_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for question whose pub_date
+        is older then 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_quesiton(self):
+        """
+        was_published_recently() returns for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
